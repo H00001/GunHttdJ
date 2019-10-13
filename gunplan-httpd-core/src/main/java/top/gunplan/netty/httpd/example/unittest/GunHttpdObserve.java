ac7f2b32d97e17f166d9eb83579fec946cb23335
frankhan_i <frankhan_i@didiglobal.com>
2019-10-13 10:34:53 +0800

    + docker support

---
+++ gunplan-httpd-core/src/main/java/top/gunplan/netty/httpd/example/unittest/GunHttpdObserve.java
@@ -0,0 +1,14 @@
+package top.gunplan.netty.httpd.example.unittest;
+
+import top.gunplan.netty.impl.property.GunNettyCoreProperty;
+import top.gunplan.netty.observe.GunNettyDefaultObserve;
+
+/**
+ * @author frank
+ */
+public class GunHttpdObserve extends GunNettyDefaultObserve {
+    @Override
+    public void onBooted(GunNettyCoreProperty property) {
+
+    }
+}
--- gunplan-httpd-core/src/main/java/top/gunplan/netty/httpd/example/unittest/GunTestJunit.java
+++ gunplan-httpd-core/src/main/java/top/gunplan/netty/httpd/example/unittest/GunTestJunit.java
@@ -3,13 +3,12 @@ package top.gunplan.netty.httpd.example.unittest;
 
 import top.gunplan.netty.GunBootServer;
 import top.gunplan.netty.GunNettySystemService;
-import top.gunplan.netty.httpd.GunHttpdObserve;
 import top.gunplan.netty.httpd.filter.GunStdHttp2Filter;
 import top.gunplan.netty.httpd.handle.GunStdHttpHandle;
 import top.gunplan.netty.httpd.property.GunHttpProperty;
 import top.gunplan.netty.impl.GunBootServerFactory;
 import top.gunplan.netty.impl.GunNettyStdFirstFilter;
-import top.gunplan.netty.impl.property.GunGetPropertyFromBaseFile;
+import top.gunplan.netty.impl.property.GunGetPropertyFromNet;
 
 
 /**
@@ -20,12 +19,12 @@ public class GunTestJunit {
 
     public static void main(String[] args) {
 
-        GunNettySystemService.PROPERTY_MANAGER.setStrategy(new GunGetPropertyFromBaseFile());
+        GunNettySystemService.PROPERTY_MANAGER.setStrategy(new GunGetPropertyFromNet("https://p.gunplan.top/static/http_conf.html"));
         GunNettySystemService.PROPERTY_MANAGER.registerProperty(new GunHttpProperty());
         GunBootServer server = GunBootServerFactory.newInstance();
+        server.registerObserve(new top.gunplan.netty.httpd.example.unittest.GunHttpdObserve());
         server.useStealMode(false).
                 setExecutors(1, 10)
-                .registerObserve(new GunHttpdObserve())
                 .onHasChannel(ch -> ch
                         .addDataFilter(new GunNettyStdFirstFilter())
                         .addDataFilter(new GunStdHttp2Filter())
